f2d260ddee0943151e8b415fa9850bec8bca5ad6
atulpat3l <patel.ap787@gmail.com>
2022-01-25 16:10:39 +0530

    Hold dice values on click so that they did not change on roll

--- src/App.js
+++ src/App.js
@@ -18,15 +18,43 @@ function App() {
   const [dice, setDice] = useState(allNewDice());
 
   const diceElements = dice.map((die) => (
-    <Die value={die.value} key={die.id} isHeld={die.isHeld} />
+    <Die
+      value={die.value}
+      key={die.id}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
   ));
 
   function handleClick() {
-    setDice(allNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld
+          ? die
+          : {
+              value: Math.ceil(Math.random() * 6),
+              isHeld: false,
+              id: nanoid(),
+            };
+      })
+    );
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
   }
 
   return (
     <main className="main">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="die__container">{diceElements}</div>
       <button className="btn" onClick={handleClick}>
         Roll
--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,12 @@
-export default function Die({ value, isHeld }) {
+export default function Die({ value, isHeld, holdDice }) {
   const styles = {
     backgroundColor: isHeld ? "#59E391" : "white",
   };
   return (
     <div className="die" style={styles}>
-      <h2 className="die__value">{value}</h2>
+      <h2 className="die__value" onClick={holdDice}>
+        {value}
+      </h2>
     </div>
   );
 }
