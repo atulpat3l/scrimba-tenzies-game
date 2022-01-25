326689d1a4541c1b0103abe470a1648bc297a5b4
atulpat3l <patel.ap787@gmail.com>
2022-01-25 12:20:22 +0530

    created ui for the game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Die from './Die';
 function App() {
+  function allNewDice () {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+ 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <div className="die__container">
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+      </div>
+    </main>
   );
 }
 
---
+++ src/Die.js
@@ -0,0 +1,7 @@
+export default function Die({value}) {
+    return (
+        <div className="die">
+        <h2 className="die__value">{value}</h2>
+    </div>
+    )
+}
